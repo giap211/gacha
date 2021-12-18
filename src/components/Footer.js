8c02d4f21bfdce3a457e1304b833069b7dfb409c
giap211 <92663267+giap211@users.noreply.github.com>
2021-12-18 11:03:11 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,52 +4,10 @@ const Footer = ({ resize }) => {
   return (
     <section id="footer">
       <div id="footer-text" style={{ textShadow: "1px 0 5px black" }}>
-        Created by Mike Li. Please give me a job thanks. Icons made by
-        <a
-          href="https://www.flaticon.com/authors/kiranshastry"
-          title="Kiranshastry"
-          style={{
-            textDecoration: "none",
-            color: "rgb(55, 61, 78)",
-            textShadow: "none",
-          }}
-        >
-          {" "}
-          Kiranshastry{" "}
-        </a>
-        and
-        <a
-          href="https://www.flaticon.com/authors/pixel-perfect"
-          title="Pixel perfect"
-          style={{
-            textDecoration: "none",
-            color: "rgb(55, 61, 78)",
-            textShadow: "none",
-          }}
-        >
-          {" "}
-          Pixel perfect{" "}
-        </a>
-        from
-        <a
-          href="https://www.flaticon.com/"
-          title="Flaticon"
-          style={{
-            textDecoration: "none",
-            color: "rgb(55, 61, 78)",
-            textShadow: "none",
-          }}
-        >
-          {" "}
-          www.flaticon.com.
-        </a>
+      ffff
+       
       </div>
-      <a
-        href="https://github.com/hbhhi/Waste-My-Primos"
-        style={{ position: "absolute", bottom: "10px", right: "10px" }}
-      >
-        {/* <img src="../assets/img/misc/github-icon.webp" alt="github-icon" /> */}
-      </a>
+      
     </section>
   );
 };
